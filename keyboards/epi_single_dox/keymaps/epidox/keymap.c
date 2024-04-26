6ec46870a280616f7007edf16eb1a433d5149218
p3dr0ck <p3dr0ck@gmx.com>
2024-04-26 15:06:00 +0300

    fixed the qwerty conditional compilation

--- keyboards/epi_single_dox/keymaps/epidox/keymap.c
+++ keyboards/epi_single_dox/keymaps/epidox/keymap.c
@@ -19,15 +19,10 @@ uint8_t val;
 #define _COLEMAK 0
 #define _LOWER 1
 #define _RAISE 2
-#define _QWERTY 3
 #define _ADJUST 16
 
 enum custom_keycode {
-#ifdef COLEMAK_LAYOUT
-    COLEMAK = SAFE_RANGE,
-#else
-  QWERTY = SAFE_RANGE,
-#endif
+  COLEMAK = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
@@ -39,6 +34,7 @@ enum custom_keycode {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+#ifdef COLEMAK_LAYOUT
 /* Colemak
  *
  * ,----------------------------------.           ,----------------------------------.
@@ -61,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,
     LSFT_T(KC_CAPS), LT(_LOWER, KC_BSPC), CTL_T(KC_SPC),     RALT_T(KC_ENT), LT(_RAISE, KC_DEL), KC_LGUI
 ),
-
+#else
 
 /* Qwerty
  *
@@ -79,13 +75,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                |      |    |      |
  *                                `------'    `------'
  */
-[_QWERTY] = LAYOUT( \
+[_COLEMAK] = LAYOUT( \
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
     LSFT_T(KC_CAPS), LT(_LOWER, KC_BSPC), CTL_T(KC_SPC),     RALT_T(KC_ENT), LT(_RAISE, KC_DEL), KC_LGUI
 ),
-
+#endif
 /* Lower
  *
  * ,----------------------------------.           ,----------------------------------.
