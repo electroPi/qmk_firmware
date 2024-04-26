62e16212f495131dbb94fc0b43363547a6531890
p3dr0ck <p3dr0ck@gmx.com>
2024-04-26 15:03:58 +0300

    added qwerty conditional compilation

--- keyboards/epi_minidox/keymaps/epidox/keymap.c
+++ keyboards/epi_minidox/keymaps/epidox/keymap.c
@@ -11,6 +11,8 @@ uint16_t hue;
 uint8_t sat;
 uint8_t val;
 
+#undef COLEMAK_LAYOUT
+#define COLEMAK_LAYOUT (1)
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -34,6 +36,7 @@ enum custom_keycode {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+#ifdef COLEMAK_LAYOUT
 /* Colemak
  *
  * ,----------------------------------.           ,----------------------------------.
@@ -56,7 +59,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_K,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,
     LSFT_T(KC_CAPS), LT(_LOWER, KC_BSPC), CTL_T(KC_SPC),     LALT_T(KC_ENT), LT(_RAISE, KC_DEL), KC_LGUI
 ),
-
+#else
+/* Qwerty
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |   ;  |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
+ * `----------------------------------'           `----------------------------------'
+ *             ,-------------------------.    ,------,-----------------.
+ *             | Shift| LOWER/Bksp| CTL  |    | Alt  | DEL/RAISE| LGUI |
+ *             | Caps |           |      |    |      |          |      |
+ *             `------------------| Space|    | ENTER|----------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_COLEMAK] = LAYOUT( \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+    LSFT_T(KC_CAPS), LT(_LOWER, KC_BSPC), CTL_T(KC_SPC),     RALT_T(KC_ENT), LT(_RAISE, KC_DEL), KC_LGUI
+),
+#endif
 
 /* Lower
  *
